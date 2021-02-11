8b7de5e216b262b847c78af5991834d1d5844c1f
mcnallc2 <mcnallc2@tcd.ie>
2021-02-11 19:36:34 +0000

    General Cleanup

--- esp32_code/WebSocketClient/WebSocketClient.ino
+++ esp32_code/WebSocketClient/WebSocketClient.ino
@@ -1,137 +1,185 @@
 
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebSocketsClient.h>
 #include <Servo.h>
 
-WebSocketsClient webSocket;
-Servo servo1;
-static const int servoPin   = 13;  // defines pin number for PWM
-static const int pushButton = 32;  // defines pin number for Button
+// Objects
+WebSocketsClient webSocket;          // WebSocket object
+Servo servo1;                        // Servo object
+
+// External Pin Defines
+static const int servoPin   = 13;    // pin for Servo PWM
+static const int pushButton = 32;    // pin for Button
+
+// Servo
+int servoToggle;                     // binary servo position variable
+#define ZERO_DEGREES 0               // 0 degrees servo angle
+#define ONE_EIGHTY_DEGREES 180       // 180 degrees servo angle 
+
+// Network
+const char *ssid     = "McNallys";   // wifi name
+const char *password = "mcnally123"; // wifi password
+bool connected = false;              // network connection status 
+
+// Timing
+unsigned long lastUpdate = millis(); // stores time(ms) since application started
+unsigned long messageInterval = 500; // time interval to prevent button bounce 
 
 
-int ServoToggle;
-#define ZERO_DEGREES 0
-#define ONE_EIGHTY_DEGREES 180
-
-const char *ssid     = "McNallys";
-const char *password = "mcnally123";
- 
-unsigned long messageInterval = 500;
-bool connected = false;
- 
-#define DEBUG_SERIAL Serial
- 
+// NOTE: likely to be depricated in the future
+// if binary is recieved we print dump in hex form
 void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
     const uint8_t* src = (const uint8_t*) mem;
-    DEBUG_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+    Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
     for(uint32_t i = 0; i < len; i++) {
         if(i % cols == 0) {
-            DEBUG_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+            Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
         }
-        DEBUG_SERIAL.printf("%02X ", *src);
+        Serial.printf("%02X ", *src);
         src++;
     }
-    DEBUG_SERIAL.printf("\n");
+    Serial.printf("\n");
 }
- 
+
+// callback for when a response is recieved
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
  
     switch(type) {
+        // if server is disconnecting
         case WStype_DISCONNECTED:
-            DEBUG_SERIAL.printf("[WSc] Disconnected!\n");
+        {
+            Serial.printf("[WSc] Disconnected!\n");
             connected = false;
+        }
             break;
-        case WStype_CONNECTED: {
-            DEBUG_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+        // if server is connnecting
+        case WStype_CONNECTED:
+        {
+            Serial.printf("[WSc] Connected to url: %s\n", payload);
             connected = true;
  
             // send message to server when Connected
-            DEBUG_SERIAL.println("[WSc] SENT: Connected");
+            Serial.println("[WSc] SENT: Connected");
             webSocket.sendTXT("Connected");
         }
-            break;
-        case WStype_TEXT:
-            DEBUG_SERIAL.printf("[WSc] RESPONSE: %s\n", payload);
-            DEBUG_SERIAL.printf("Response Recieved - Toggling Servo ...\n");
-            if(ServoToggle){
-              servo1.write(ZERO_DEGREES);
+        break;
+
+        // if a response message is recieved
+        // this is where we toggle the servo position
+        case WStype_TEXT:{
+            Serial.printf("[WSc] RESPONSE: %s\n", payload);
+            Serial.printf("-> Response Recieved - Toggling Servo ...\n");
+
+            // position the servo
+            if(servoToggle){
+                servo1.write(ONE_EIGHTY_DEGREES);
             }
             else{
-              servo1.write(ONE_EIGHTY_DEGREES);
+                servo1.write(ZERO_DEGREES);
             }
-            ServoToggle = ~ServoToggle;
-            break;
+            
+            // toggle the postion indicator
+            servoToggle = ~servoToggle;
+        }
+        break;
+
+        // if binary is receieved from server
         case WStype_BIN:
-            DEBUG_SERIAL.printf("[WSc] get binary length: %u\n", length);
+        {
+            Serial.printf("[WSc] get binary length: %u\n", length);
+
+            // dump binary in hex form
             hexdump(payload, length);
-            break;
+        }
+        break;
+        
+        // if we get a ping
         case WStype_PING:
+        {
             // pong will be send automatically
-            DEBUG_SERIAL.printf("[WSc] get ping\n");
-            break;
+            Serial.printf("[WSc] get ping\n");
+        }
+        break;
+
+        // if we get a pong
         case WStype_PONG:
-            // answer to a ping we send
-            DEBUG_SERIAL.printf("[WSc] get pong\n");
-            break;
-    case WStype_ERROR:
-    case WStype_FRAGMENT_TEXT_START:
-    case WStype_FRAGMENT_BIN_START:
-    case WStype_FRAGMENT:
-    case WStype_FRAGMENT_FIN:
+        {
+            // no action required, answer to a ping we send
+            Serial.printf("[WSc] get pong\n");
+        }
+        break;
+
+        // other possible cases we do nothing
+        case WStype_ERROR:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:
+
         break;
     }
 }
- 
-void setup() {
-    DEBUG_SERIAL.begin(115200);
 
-    ServoToggle = 0;
-    servo1.attach(servoPin);
-    pinMode(pushButton, INPUT);
-    
-    DEBUG_SERIAL.println();
-    DEBUG_SERIAL.println();
-    DEBUG_SERIAL.println();
- 
+
+// setup run once when application starts
+void setup() {
+
+    // start serial monitor
+    Serial.begin(115200);
+
+    // 4 seconds to allow the device to boot fully
+    Serial.println("\n\n");
     for(uint8_t t = 4; t > 0; t--) {
-        DEBUG_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-        DEBUG_SERIAL.flush();
+        Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+        Serial.flush();
         delay(1000);
     }
- 
-    WiFi.begin(ssid, password);
- 
+
+    pinMode(servoPin, OUTPUT);  // set servo PWM pin to output mode
+    pinMode(pushButton, INPUT); // set pushbutton pin to input mode
+    
+    servoToggle = 0;            // init servo to position 0
+    servo1.attach(servoPin);    // set ouptut PWM pin for servo object
+    
+    WiFi.begin(ssid, password); // connect device to wifi network
+
+    // poll connection status to ensure connection is made
     while ( WiFi.status() != WL_CONNECTED ) {
-      delay ( 500 );
-      DEBUG_SERIAL.print ( "." );
+        delay(500);
+        Serial.print(".");
     }
     
-    DEBUG_SERIAL.print("Local IP: "); DEBUG_SERIAL.println(WiFi.localIP());
+    // print device IP address
+    Serial.print("Local IP: ");
+    Serial.println(WiFi.localIP());
     
     // server address, port and URL
     // just using this websocket service for now to get a default response
     webSocket.begin("echo.websocket.org", 80, "/");
  
-    // event handler
+    // set websocket event handler
     webSocket.onEvent(webSocketEvent);
 }
- 
-unsigned long lastUpdate = millis();
- 
 
+
+// loop is basically our embedded while(1)
 void loop() {
+    
     webSocket.loop();
 
     // if connected, button is pushed, and message interval is reached we send message to server
     // try press and release button in under 500ms, otherwise it will start to spam the server
     if (connected && digitalRead(pushButton) && lastUpdate+messageInterval<millis()){
-        DEBUG_SERIAL.println("\n-> Button Pressed - Sending Message ...");
-        DEBUG_SERIAL.println("[WSc] SENT: Simple js client message!!");
+        Serial.println("\n-> Button Pressed - Sending Message ...");
+        Serial.println("[WSc] SENT: Simple js client message!!");
+
+        // sending messege to server
         webSocket.sendTXT("Simple js client message!!");
-        DEBUG_SERIAL.println("-> Message Sent - Waiting for Response ...");
-        
+        Serial.println("-> Message Sent - Waiting for Response ...");
+
+        // update time since previous event
         lastUpdate = millis();
     }
 }
