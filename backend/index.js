458be24814558056367f2a9ce582834a36a76e6b
conor heffernan <conorheffernan@hubspot.com>
2021-02-11 19:19:59 +0000

    set up end point for the app

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,33 @@
+/* requirements */
 const express = require("express");
-const port = 8080;
+const port = process.env.PORT || 3000;
+
+/*
+server definition and config
+*/
 
 const server = express();
 
+/*
+end points
+*/
+
 server.get("/healthcheck", (req, res) => {
-	res.send({ message: "server running..." });
+	res.send({ message: "auth server 1 is running" });
 });
 
+server.get("/", (req, res, next) => {
+	next("Error: Not found. Please specify a valid endpoint");
+});
+
+server.get("/toggleLatch", (req, res) => {
+	const desiredState = req.query && req.query.state;
+	res.send({ Authorization: "ok", newDoorState: desiredState });
+});
+
+/*
+activate server
+*/
 server.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+	console.log(`Server is now running on port ${port}`);
 });
