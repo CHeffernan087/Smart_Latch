8061c752e634cd464af38345a4196e687a297535
thomaskelly97 <tomskelly97@gmail.com>
2021-02-19 09:37:03 +0000

    working, fixing verification

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const {OAuth2Client} = require('google-auth-library');
 // todo: move this to json file
-const APP_GOOGLE_CLIENT_ID = "203181786221-4rllfugkn3o5ulgdn9gtpags73tbek1g.apps.googleusercontent.com";
+const APP_GOOGLE_CLIENT_ID = "203181786221-3uljiupllmu130gv7o6nei0c0vsuvb70.apps.googleusercontent.com";
 
 /*
 server definition and config
@@ -34,22 +34,16 @@ app.get("/toggleLatch", (req, res) => {
 
 app.get("/verifyUser", (req, res, next) => {
 	const client = new OAuth2Client(APP_GOOGLE_CLIENT_ID);
-	let payload; // lets just send the payload back for now to see what it is 
-	
-	// ugly for the moment, just trying to see how the java POST is built...
-	let token = req.query.idToken;
-	console.log(`Query: ${JSON.stringify(req.query)}`);
-	console.log(`Params: ${JSON.stringify(req.params)}`);
-	console.log(`Body: ${JSON.stringify(req.body)}`);
+	const token = req.query.idToken;
 	
 	async function verify() {
 	const ticket = await client.verifyIdToken({
 		idToken: token,
 		audience: APP_GOOGLE_CLIENT_ID, 
 	});
-	payload = ticket.getPayload();
-	console.log(payload);
+	const payload = ticket.getPayload();
 	const userid = payload['sub'];
+	console.log(`Payload: ${payload}`)
 	// use userid as a unique identifer for the user in our database 
 
 	}
