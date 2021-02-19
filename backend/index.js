5d582275ffe9fea8e09e5b6e4aaaace9e1a68bd7
thomaskelly97 <tomskelly97@gmail.com>
2021-02-19 00:16:08 +0000

    trying to figure out how to get params...

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,12 @@ app.post("/verifyUser", (req, res, next) => {
 	let payload; // lets just send the payload back for now to see what it is 
 	
 	// ugly for the moment, just trying to see how the java POST is built...
-	let token = req.query.idToken;
+	// let token = req.query.idToken;
+	console.log(`Query: ${JSON.stringify(req.query)}`);
+	console.log(`Params: ${JSON.stringify(req.params)}`);
+	console.log(`Body: ${JSON.stringify(req.body)}`);
+
 	
-	console.log(`TOKEN : ${token}`)
 	
 	async function verify() {
 	const ticket = await client.verifyIdToken({
