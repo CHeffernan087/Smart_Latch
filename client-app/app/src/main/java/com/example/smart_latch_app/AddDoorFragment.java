5c14b5ed67002a996b0a1133bc04ceef257d47ca
conor heffernan <conorheffernan@hubspot.com>
2021-03-04 21:34:29 +0000

    removed todo

--- client-app/app/src/main/java/com/example/smart_latch_app/AddDoorFragment.java
+++ client-app/app/src/main/java/com/example/smart_latch_app/AddDoorFragment.java
@@ -75,10 +75,6 @@ public class AddDoorFragment extends DialogFragment {
 
         OkHttpClient client = new OkHttpClient();
         String hostUrl = getString(R.string.smart_latch_url) + "/registerDoor" ;
-        // todo grab the email of the user currently logged in
-        // to change the door you want to register update the email here and change the door
-        // id in detectTagData method. Add yourself to the doors in the comment so that
-        // other people can add themselves to available doors
 
         String email ="";
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(((MyDoorsActivity) getActivity()));
