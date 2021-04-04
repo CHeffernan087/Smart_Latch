050d0611d26a290c42ec67b00615cc752fc2e7c5
Owen Burke <35539701+BurkeO@users.noreply.github.com>
2021-04-04 18:54:44 +0100

    adding init version of endpoint to get db vectors

--- backend/google_vm_recognition/main.py
+++ backend/google_vm_recognition/main.py
@@ -32,11 +32,14 @@ class NoFaceFoundException(Exception):
 class NoDocumentFoundException(Exception):
     pass
 
+class NoVectorFoundException(Exception):
+    pass
+
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
-    storage_client = storage.Client()   
-    # storage_client = storage.Client.from_service_account_json("smart-latch-fd041937391e.json")
+    # storage_client = storage.Client()   
+    storage_client = storage.Client.from_service_account_json("smart-latch-fd041937391e.json")
     bucket = storage_client.bucket(bucket_name)
 
     blob = bucket.blob(source_blob_name)
@@ -47,10 +50,10 @@ MTCNN = mtcnn.MTCNN()
 VGG_FACE = VGGFace(model='resnet50', include_top=False,
                    input_shape=(224, 224, 3), pooling='avg')
 download_blob("vggface-bucket", "embeddings.csv", "embeddings.csv")
-EMBEDDINGS = pandas.read_csv("embeddings.csv")
-EMBEDDINGS_2D_LIST = EMBEDDINGS.values.tolist()
-DB = firestore.Client()
-# DB = firestore.Client.from_service_account_json("smart-latch-fd041937391e.json")
+VECTORS = pandas.read_csv("embeddings.csv")
+VECTORS_2D_LIST = VECTORS.values.tolist()
+# DB = firestore.Client()
+DB = firestore.Client.from_service_account_json("smart-latch-fd041937391e.json")
 
 @app.route("/check")
 def check():
@@ -65,7 +68,7 @@ def register_face():
         img_file = flask.request.files['image']
         img_file.save(image_path_str)
         image = cv2.imread(image_path_str)
-        face_vector = get_embedding(image)
+        face_vector = get_face_vector(image)
         doc_ref = DB.collection('Users').document(email)
         user_doc = doc_ref.get()
         if user_doc.exists is False:
@@ -73,13 +76,7 @@ def register_face():
         doc_ref.update({
             'face_vector' : face_vector.tolist()[0]
         })
-        # with open("uploaded.txt", "w") as uploaded_file:
-        #     uploaded_file.write(str(face_vector.tolist()[0]))
-
         fields_dict = doc_ref.get().to_dict()
-
-        # with open("gotten.txt", "w") as gotten_file:
-        #     gotten_file.write(str(fields_dict['face_vector']))
         if 'face_vector' in fields_dict and len(fields_dict['face_vector']) == VECTOR_SIZE:
             response = jsonify({"Message" : f"Successfully wrote {len(fields_dict['face_vector'])} vector to user {email}"}), 200
         else:
@@ -103,17 +100,69 @@ def facial_recognition():
             temp = base64.b64encode(decoded_image_data).decode('ascii').encode()
             file_to_save.write(base64.decodebytes(temp))
 
-        # with open("string.txt", "w") as text_file:
-        #     text_file.write(base64.b64encode(decoded_image_data).decode('ascii'))
-
         img = cv2.imread(img_path_str)
-        candidate_embedding = get_embedding(img)
+        face_vector = get_face_vector(img)
         lowest_score_person = None
         lowest_score = maxsize
-        for embedding_list_and_person in EMBEDDINGS_2D_LIST:
+        for vector_list_and_person in VECTORS_2D_LIST:
+            embedding_list = vector_list_and_person[:-1]
+            person = vector_list_and_person[-1]
+            score = get_match_score(embedding_list, face_vector)
+            if score < lowest_score:
+                lowest_score_person = person
+                lowest_score = score
+        if lowest_score <= THRESHOLD:
+            response = jsonify({'Person': lowest_score_person, 'Score': lowest_score})
+        else:
+            response = jsonify({"Error": "Couldn't classify image"})
+    except NoFaceFoundException:
+        response = jsonify({"Error": "No Face Found In Image"})
+    except Exception as e:
+        response = jsonify({"Error": str(traceback.format_exc())})
+    finally:
+        if os.path.exists(img_path_str):
+            os.remove(img_path_str)
+        return response
+
+@app.route("/verify", methods=["POST"])
+def verify_face():
+    response = jsonify({"Error": "None"})
+    try:
+        img_path_str = f"{time()}_received.png"
+        with open(img_path_str, 'wb') as file_to_save:
+            decoded_image_data = base64.b64decode(flask.request.json["image"])
+            temp = base64.b64encode(decoded_image_data).decode('ascii').encode()
+            file_to_save.write(base64.decodebytes(temp))
+
+        door_id = flask.request.json["door"]
+        doc_ref = DB.collection('Doors').document(str(door_id))
+        user_doc = doc_ref.get()
+        if user_doc.exists is False:
+            raise NoDocumentFoundException(f"No door for {door_id}")
+        fields_dict = user_doc.to_dict()
+        embeddings_2d_list = []
+        authorized_users_refs = fields_dict['Authorised']
+        for user_refs in list(authorized_users_refs):
+            user_doc = user_refs.get().to_dict()
+            #
+            if user_doc is None:
+                continue
+            #
+            if "face_vector" not in user_doc:
+                # raise NoVectorFoundException(f"User {user_doc['email']} has no face vector")
+                continue
+            vector = user_doc['face_vector']
+            vector.append(f"{user_doc['firstname']}_{user_doc['lastname']}")
+            embeddings_2d_list.append(vector)
+
+        img = cv2.imread(img_path_str)
+        face_vector = get_face_vector(img)
+        lowest_score_person = None
+        lowest_score = maxsize
+        for embedding_list_and_person in embeddings_2d_list:
             embedding_list = embedding_list_and_person[:-1]
             person = embedding_list_and_person[-1]
-            score = get_match_score(embedding_list, candidate_embedding)
+            score = get_match_score(embedding_list, face_vector)
             if score < lowest_score:
                 lowest_score_person = person
                 lowest_score = score
@@ -152,7 +201,7 @@ def get_match_score(known_embedding, candidate_embedding):
     return cosine(known_embedding, candidate_embedding)
 
 
-def get_embedding(image):
+def get_face_vector(image):
     # faces = [cv2.imread((str(f)))for f in filenames]
     faces = [extract_face(image)]
     samples = asarray(faces, 'float32')
--- backend/google_vm_recognition/test.py
+++ backend/google_vm_recognition/test.py
@@ -9,17 +9,25 @@ with open("string.txt", "w") as text_file:
     text_file.write(encoded_string)
 
 data = {"image": encoded_string}
-# url = "http://127.0.0.1:9090/recog"
-url = "https://recognition.smart-latchxyz.xyz/"
+url = "http://127.0.0.1:8080/"
+# url = "https://recognition.smart-latchxyz.xyz/"
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, json=data, headers=headers)
 print(response.json())
 
 
-data = {"email": "andrew.hodges@gmail.com"}
-# url = "http://127.0.0.1:8080/register"
-url = "https://recognition.smart-latchxyz.xyz/register"
-files = {'image': open('Owen.jpg', 'rb')}
+data = {"image": encoded_string, "door": 31415}
+url = "http://127.0.0.1:8080/verify"
+# url = "https://recognition.smart-latchxyz.xyz/verify"
+headers = {'Content-type': 'application/json'}
+response = requests.post(url, json=data, headers=headers)
+print(response.json())
+
+
+data = {"email": "mcnallc2@tcd.ie"}
+url = "http://127.0.0.1:8080/register"
+# url = "https://recognition.smart-latchxyz.xyz/register"
+files = {'image': open('McNally.jpg', 'rb')}
 response = requests.post(url, files=files, data=data)
 print(response.status_code)
 print(response.json())
