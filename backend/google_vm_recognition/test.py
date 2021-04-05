84a1c1c4c4ce27ee0b1a919f6a4c9708f82bd8b5
Owen Burke <35539701+BurkeO@users.noreply.github.com>
2021-04-05 11:40:29 +0100

    updates to test script

--- backend/google_vm_recognition/test.py
+++ backend/google_vm_recognition/test.py
@@ -8,25 +8,25 @@ with open("McNally.jpg", "rb") as image_file:
 with open("string.txt", "w") as text_file:
     text_file.write(encoded_string)
 
-data = {"image": encoded_string}
-url = "http://127.0.0.1:8080/"
+# data = {"image": encoded_string}
+# # url = "http://127.0.0.1:8080/"
 # url = "https://recognition.smart-latchxyz.xyz/"
-headers = {'Content-type': 'application/json'}
-response = requests.post(url, json=data, headers=headers)
-print(response.json())
+# headers = {'Content-type': 'application/json'}
+# response = requests.post(url, json=data, headers=headers)
+# print(response.json())
 
 
-data = {"image": encoded_string, "door": 31415}
-url = "http://127.0.0.1:8080/verify"
+# data = {"image": encoded_string, "door": 31415}
+# # url = "http://127.0.0.1:8080/verify"
 # url = "https://recognition.smart-latchxyz.xyz/verify"
-headers = {'Content-type': 'application/json'}
-response = requests.post(url, json=data, headers=headers)
-print(response.json())
+# headers = {'Content-type': 'application/json'}
+# response = requests.post(url, json=data, headers=headers)
+# print(response.json())
 
 
 data = {"email": "mcnallc2@tcd.ie"}
-url = "http://127.0.0.1:8080/register"
-# url = "https://recognition.smart-latchxyz.xyz/register"
+# url = "http://127.0.0.1:8080/register"
+url = "https://recognition.smart-latchxyz.xyz/register"
 files = {'image': open('McNally.jpg', 'rb')}
 response = requests.post(url, files=files, data=data)
 print(response.status_code)
