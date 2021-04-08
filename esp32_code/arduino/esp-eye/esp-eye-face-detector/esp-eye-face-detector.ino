02260fd74b220d662fa332f0765e1bdac79ae69c
conor heffernan <conorheffernan@hubspot.com>
2021-04-08 14:39:00 +0100

    moved the lock update to the appropriate place

--- esp32_code/arduino/esp-eye/esp-eye-face-detector/esp-eye-face-detector.ino
+++ esp32_code/arduino/esp-eye/esp-eye-face-detector/esp-eye-face-detector.ino
@@ -22,17 +22,17 @@
 #define CAMERA_MODEL_ESP_EYE
 
 // timeouts
-#define MOTION_TIMEOUT     10000
-#define NFC_TIMEOUT        10000
+#define MOTION_TIMEOUT 10000
+#define NFC_TIMEOUT 10000
 #define FACE_RECOG_TIMEOUT 10000
 
 // face recognision server POST URL
-const char* img_recog_url = "http://recognition.smart-latchxyz.xyz/";
+const char *img_recog_url = "http://recognition.smart-latchxyz.xyz/";
 // test POST url
 //const char* img_recog_url = "http://httpbin.org/post";
 
 // websocket server URL - health_check: http://smart-latchxyz.xyz/healthcheck/
-const char* wss_url = "ws.smart-latchxyz.xyz";
+const char *wss_url = "ws.smart-latchxyz.xyz";
 // test WSS url
 //const char* wss_url = "echo.websocket.org";
 
@@ -40,7 +40,7 @@ const char* wss_url = "ws.smart-latchxyz.xyz";
 // All F's sends to all boards on network
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 //String myMACAddress     = "AC67B267A19C";
-String myMACAddress     = "31415";
+String myMACAddress = "31415";
 
 // websocket
 WebSocketsClient webSocket; // websocket object
@@ -50,25 +50,26 @@ bool connected = false;     // websocket connection status
 OTA ota_updater("1.0", "esp-eye", "https://europe-west2-smart-latch.cloudfunctions.net/getDownloadUrl");
 
 // motion detection
-bool motionDetected = false;  // motion detection status
+bool motionDetected = false; // motion detection status
 
 // verification flags
 bool faceRecogFlag = false;
-bool nfcFlag       = false;
+bool nfcFlag = false;
 
 // http client
 HTTPClient http;
-        
+
 // timeout variables
-unsigned long mdStartTime  = millis();
-unsigned long mdCurrTime   = millis();
-unsigned long frStartTime  = millis();
-unsigned long frCurrTime   = millis();
+unsigned long mdStartTime = millis();
+unsigned long mdCurrTime = millis();
+unsigned long frStartTime = millis();
+unsigned long frCurrTime = millis();
 unsigned long nfcStartTime = millis();
-unsigned long nfcCurrTime  = millis();
+unsigned long nfcCurrTime = millis();
 
 // config face detection model parameters
-static inline mtmn_config_t app_mtmn_config(){
+static inline mtmn_config_t app_mtmn_config()
+{
   mtmn_config_t mtmn_config = {0};
   mtmn_config.type = FAST;
   mtmn_config.min_face = 80;
@@ -88,7 +89,8 @@ static inline mtmn_config_t app_mtmn_config(){
 mtmn_config_t mtmn_config = app_mtmn_config();
 
 // structure of send data
-typedef struct struct_message {
+typedef struct struct_message
+{
   char a[32];
   String b;
   bool c;
@@ -98,13 +100,15 @@ typedef struct struct_message {
 struct_message myData;
 
 // callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
   Serial.print("[ESP-NOW] Last Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len){
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("\n[ESP-NOW] Packet Recieved:\n");
   Serial.print("Bytes received: ");
@@ -118,59 +122,67 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len){
   Serial.println();
 
   // if string is motion and bool is true
-  if(myData.c && (myData.b == "motion")){
+  if (myData.c && (myData.b == "motion"))
+  {
     Serial.println("[ESP-NOW] Motion Detected\n");
     motionDetected = true;
     mdStartTime = millis();
-    mdCurrTime  = millis();
+    mdCurrTime = millis();
   }
 
   // if string is closed and bool is true
-  if(myData.b == "latch"){
-    if (myData.c) {
+  if (myData.b == "latch")
+  {
+    if (myData.c)
+    {
       Serial.println("[ESP-NOW] Latch Opened\n");
       // sending person name over websocket
       Serial.println("[WSc] Notifying Cloud");
-      webSocket.sendTXT("opened");
+      webSocket.sendTXT("message:latchUpdate,status:opened");
     }
-    else {
+    else
+    {
       Serial.println("[ESP-NOW] Latch Closed\n");
       // sending person name over websocket
       Serial.println("[WSc] Notifying Cloud");
-      webSocket.sendTXT("closed");
+      webSocket.sendTXT("message:latchUpdate,status:closed");
     }
   }
-  
 }
 
 // function to change latch state over esp-now
-void changeLatchState(void){
+void changeLatchState(void)
+{
   // Set motion values to send
   strcpy(myData.a, "ESP-EYE -> ESP32");
   myData.b = "toggle";
   myData.c = true;
 
   // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
   Serial.print("[ESP-NOW] esp-eye -> esp32: sending string - ");
   Serial.println(myData.b);
 
-  if (result == ESP_OK) {
+  if (result == ESP_OK)
+  {
     Serial.println("[ESP-NOW] Sent with success");
   }
-  else {
+  else
+  {
     Serial.println("[ESP-NOW] Error sending the data");
   }
 }
 
-
 // NOTE: likely to be depricated in the future
 // if binary is recieved we print dump in hex form
-void hexdump(const void *mem, uint32_t len, uint8_t cols = 16){
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
+{
   const uint8_t *src = (const uint8_t *)mem;
   Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
-  for (uint32_t i = 0; i < len; i++){
-    if (i % cols == 0){
+  for (uint32_t i = 0; i < len; i++)
+  {
+    if (i % cols == 0)
+    {
       Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
     }
     Serial.printf("%02X ", *src);
@@ -180,57 +192,62 @@ void hexdump(const void *mem, uint32_t len, uint8_t cols = 16){
 }
 
 // callback for when a response is recieved
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
 
-  switch(type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[WSc] Disconnected!\n");
-      connected = false;
-      break;
-    case WStype_CONNECTED:
-      Serial.printf("[WSc] Connected to url: %s\n", payload);
-      connected = true;
-      // send message to server when Connected
-      Serial.println("[WSc] SENT: Connected");
-      
-      break;
-    case WStype_TEXT:
-      Serial.printf("\nResponse Recieved\n");
-      Serial.printf("[WSc] RESPONSE: %s\n", payload);
-      if (!memcmp(payload, "ToggleLatch", length)){
-        Serial.printf("[WSc] NFC Verified\n", payload);
-        nfcFlag = true;
-        nfcStartTime = millis();
-      }
-      if (!memcmp(payload, "boardIdReq", length)){
-        Serial.printf("[WSc] Sending Board Id - ");
-        Serial.println(myMACAddress);
-        String boardIdResp = "message:boardIdRes,doorId:" + myMACAddress;
-        webSocket.sendTXT(boardIdResp);
-      }
-      break;
-    case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      hexdump(payload, length);
-      break;
-    case WStype_PING:
-      // pong will be send automatically
-      Serial.printf("[WSc] get ping\n");
-      break;
-    case WStype_PONG:
-      // answer to a ping we send
-      Serial.printf("[WSc] get pong\n");
-      break;
-    case WStype_ERROR:
-    case WStype_FRAGMENT_TEXT_START:
-    case WStype_FRAGMENT_BIN_START:
-    case WStype_FRAGMENT:
-    case WStype_FRAGMENT_FIN:
-      break;
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[WSc] Disconnected!\n");
+    connected = false;
+    break;
+  case WStype_CONNECTED:
+    Serial.printf("[WSc] Connected to url: %s\n", payload);
+    connected = true;
+    // send message to server when Connected
+    Serial.println("[WSc] SENT: Connected");
+
+    break;
+  case WStype_TEXT:
+    Serial.printf("\nResponse Recieved\n");
+    Serial.printf("[WSc] RESPONSE: %s\n", payload);
+    if (!memcmp(payload, "ToggleLatch", length))
+    {
+      Serial.printf("[WSc] NFC Verified\n", payload);
+      nfcFlag = true;
+      nfcStartTime = millis();
+    }
+    if (!memcmp(payload, "boardIdReq", length))
+    {
+      Serial.printf("[WSc] Sending Board Id - ");
+      Serial.println(myMACAddress);
+      String boardIdResp = "message:boardIdRes,doorId:" + myMACAddress;
+      webSocket.sendTXT(boardIdResp);
+    }
+    break;
+  case WStype_BIN:
+    Serial.printf("[WSc] get binary length: %u\n", length);
+    hexdump(payload, length);
+    break;
+  case WStype_PING:
+    // pong will be send automatically
+    Serial.printf("[WSc] get ping\n");
+    break;
+  case WStype_PONG:
+    // answer to a ping we send
+    Serial.printf("[WSc] get pong\n");
+    break;
+  case WStype_ERROR:
+  case WStype_FRAGMENT_TEXT_START:
+  case WStype_FRAGMENT_BIN_START:
+  case WStype_FRAGMENT:
+  case WStype_FRAGMENT_FIN:
+    break;
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println();
 
@@ -258,11 +275,14 @@ void setup() {
   config.pixel_format = PIXFORMAT_JPEG;
 
   //init with high specs to pre-allocate larger buffers
-  if (psramFound()) {
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10; //0-63 lower number means higher quality 
+    config.jpeg_quality = 10; //0-63 lower number means higher quality
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12; //0-63 lower number means higher quality
     config.fb_count = 1;
@@ -275,22 +295,23 @@ void setup() {
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
   // set frame size
-  sensor_t * s = esp_camera_sensor_get();
+  sensor_t *s = esp_camera_sensor_get();
   s->set_framesize(s, FRAMESIZE_QVGA);
 
   // init boot process
-  for(uint8_t t = 4; t > 0; t--) {
+  for (uint8_t t = 4; t > 0; t--)
+  {
     Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
     Serial.flush();
     delay(1000);
   }
-
 
   // Set the device as a Station and Soft Access Point simultaneously
   WiFi.mode(WIFI_AP_STA);
@@ -298,7 +319,8 @@ void setup() {
   // Set device as a Wi-Fi Station
   WiFi.begin(); // this connects to the last used WiFi network
   //  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("[WiFi] Setting as a Wi-Fi Station..");
   }
@@ -308,11 +330,12 @@ void setup() {
   Serial.println(WiFi.channel());
   Serial.print("[WiFi] Board MAC Address: ");
   Serial.println(WiFi.macAddress());
-  
+
   myMACAddress = WiFi.macAddress();
-  
+
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("[ESP-NOW] Error initializing ESP-NOW");
     return;
   }
@@ -332,7 +355,8 @@ void setup() {
   peerInfo.encrypt = false;
 
   // Add peer device
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
     Serial.println("Failed to add peer");
     return;
   }
@@ -341,7 +365,8 @@ void setup() {
   WiFi.begin();
 
   // connection delay
-  while ( WiFi.status() != WL_CONNECTED ) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -361,18 +386,19 @@ void setup() {
   ota_updater.checkForUpdates();
 }
 
+size_t frame_num = 0;                // init frame number
+camera_fb_t *fb = NULL;              // allocate image mem
+dl_matrix3du_t *image_matrix = NULL; // allocate image matrix RGB mem
 
-size_t frame_num = 0;                 // init frame number
-camera_fb_t *fb = NULL;               // allocate image mem
-dl_matrix3du_t *image_matrix = NULL;  // allocate image matrix RGB mem
-
-void loop() {
+void loop()
+{
 
   // websocket object update must be called every loop
   webSocket.loop();
 
   // if motion is detected we begin verification flow for face recognision
-  if (connected && motionDetected){
+  if (connected && motionDetected)
+  {
 
     // get current time
     int64_t start_time = esp_timer_get_time();
@@ -380,7 +406,8 @@ void loop() {
     // get one image with camera
     fb = esp_camera_fb_get();
     // if image was not retrieved
-    if (!fb){
+    if (!fb)
+    {
       Serial.println("[ESP-CAM] Camera capture failed");
       return;
     }
@@ -388,14 +415,15 @@ void loop() {
     // get time again
     int64_t fb_get_time = esp_timer_get_time();
     Serial.printf("[ESP-CAM] Get one frame in %lld ms.\n", (fb_get_time - start_time) / 1000);
-    
+
     // allocate image matrix to store RGB data
     image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
 
     // transform image to RGB
     uint32_t res = fmt2rgb888(fb->buf, fb->len, fb->format, image_matrix->item);
     // could not transfrom to RGB
-    if (true != res){
+    if (true != res)
+    {
       Serial.printf("[ESP-CAM] fmt2rgb888 failed, fb: %d\n", fb->len);
       dl_matrix3du_free(image_matrix);
       return;
@@ -408,44 +436,47 @@ void loop() {
     box_array_t *net_boxes = face_detect(image_matrix, &mtmn_config);
     // print detection time in ms
 
-    Serial.printf("[ESP-CAM] Detection time consumption: %lldms\n", (esp_timer_get_time() - fb_get_time) / 1000);        
+    Serial.printf("[ESP-CAM] Detection time consumption: %lldms\n", (esp_timer_get_time() - fb_get_time) / 1000);
 
     // if face was detected, boxes would be generated
-    if (net_boxes){
-      
-      Serial.println("[ESP-CAM] Face Detected");        
- 
-      if(WiFi.status()== WL_CONNECTED){
-        
+    if (net_boxes)
+    {
+
+      Serial.println("[ESP-CAM] Face Detected");
+
+      if (WiFi.status() == WL_CONNECTED)
+      {
+
         // sedning image bin over http post request
         http.begin(img_recog_url);
 
         // base64 encoding of the image
-        String img_buffer = base64::encode((uint8_t *) fb->buf, fb->len);
+        String img_buffer = base64::encode((uint8_t *)fb->buf, fb->len);
         // generating json payload
         String payload = "{\"image\": \"" + img_buffer + "\", \"door\": \"" + myMACAddress + "\"}";
 
         // sending image to recognition server in HTTP POST request
-        Serial.println("[HTTP-POST] Sending image payload...\n"); 
+        Serial.println("[HTTP-POST] Sending image payload...\n");
         Serial.println(payload);
-        http.addHeader("Content-Type", "application/json");     
+        http.addHeader("Content-Type", "application/json");
         int httpResponseCode = http.POST(payload);
 
         // if error free response recieved
-        if (httpResponseCode > 0){
+        if (httpResponseCode > 0)
+        {
 
           Serial.print("[HTTP-POST] Response Code: ");
           Serial.println(httpResponseCode);
-          
+
           // parsing the response data
-          String response = http.getString();              // get response data
+          String response = http.getString(); // get response data
           Serial.print("[HTTP-POST] Response: ");
           Serial.println(response);
-          
-          int ind1 = response.indexOf(',');                // finds location of first comma
-          String person = response.substring(1, ind1);     // captures person data String
-          int ind2 = response.indexOf(',', ind1+1 );       // finds location of second comma
-          String score = response.substring(ind1+1, ind2); // captures scorre data String
+
+          int ind1 = response.indexOf(',');                  // finds location of first comma
+          String person = response.substring(1, ind1);       // captures person data String
+          int ind2 = response.indexOf(',', ind1 + 1);        // finds location of second comma
+          String score = response.substring(ind1 + 1, ind2); // captures scorre data String
 
           Serial.println("[HTTP-POST] Facial Recog Results:");
           Serial.print("person = ");
@@ -455,53 +486,60 @@ void loop() {
           Serial.println();
 
           // if person data string actually has a persons name
-          if (person.substring(4, 10) == "Person"){
+          if (person.substring(4, 10) == "Person")
+          {
             Serial.println("[HTTP-POST] Facial Recognision Verified!");
             // reset motion status flag
             motionDetected = false; // finished with motion detected
-            faceRecogFlag  = true;  // asserting facial recog flag
-            frStartTime    = millis();  // starting facial recog timeout
+            faceRecogFlag = true;   // asserting facial recog flag
+            frStartTime = millis(); // starting facial recog timeout
           }
         }
-        else{
+        else
+        {
           Serial.print("[HTTP-POST] Error on sending POST: ");
           Serial.println(httpResponseCode);
         }
         // free resources
         http.end();
       }
-      else{
-        Serial.println("[WiFi] Disconnected");  
+      else
+      {
+        Serial.println("[WiFi] Disconnected");
       }
     }
     // free image allcoation mem
     dl_matrix3du_free(image_matrix);
   }
 
-  // motionDetection timeout    
+  // motionDetection timeout
   mdCurrTime = millis();
-  if ((mdCurrTime - mdStartTime > MOTION_TIMEOUT) && motionDetected){
+  if ((mdCurrTime - mdStartTime > MOTION_TIMEOUT) && motionDetected)
+  {
     Serial.println("[TIMEOUT] Motion Detection Timeout\n");
     motionDetected = false;
   }
 
-  // nfc verification timeout    
+  // nfc verification timeout
   nfcCurrTime = millis();
-  if ((nfcCurrTime - nfcStartTime > NFC_TIMEOUT) && nfcFlag){
+  if ((nfcCurrTime - nfcStartTime > NFC_TIMEOUT) && nfcFlag)
+  {
     Serial.println("[TIMEOUT] NFC Timeout\n");
     nfcFlag = false;
   }
-  
-  // facial recognision timeout    
+
+  // facial recognision timeout
   frCurrTime = millis();
-  if ((frCurrTime - frStartTime > FACE_RECOG_TIMEOUT) && faceRecogFlag){
+  if ((frCurrTime - frStartTime > FACE_RECOG_TIMEOUT) && faceRecogFlag)
+  {
     Serial.println("[TIMEOUT] Facial Recog Timeout\n");
     faceRecogFlag = false;
   }
 
   // 2FA - and operation on both verifation flags :)
-  if (nfcFlag && faceRecogFlag){
-//  if (faceRecogFlag){
+  if (nfcFlag && faceRecogFlag)
+  {
+    //  if (faceRecogFlag){
     changeLatchState();
     nfcFlag = false;
     faceRecogFlag = false;
